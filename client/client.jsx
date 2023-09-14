628d36beab5a24f3a624b929de0857a039669214
taylorhelene <taylorhelene09@gmail.com>
2023-09-14 20:17:12 +0300

    xyz

---
+++ client/client.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App =()=>{
+    return(
+        <div>
+            React Component
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>,document.querySelector("#Container"))
---
+++ server/index.js
@@ -0,0 +1,18 @@
+// this was befor we added babelrc file, we can now use es6 scripts const express = require ('express');
+import express from 'express';
+import {readFileSync} from 'fs';
+
+const app = new express();
+
+app.use(express.static("dist"));
+app.get('/',async (_req,res)=>{
+    const index = readFileSync('public/index.html','utf8')
+    res.send(index);
+    //res.send(
+     //   '<h1>REACTING</h1>'
+    //)
+
+})
+
+app.listen(7777);
+console.info("listen");
