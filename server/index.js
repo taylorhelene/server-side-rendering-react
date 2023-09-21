41d46e3702ac4dd263333fe1b17e5296c0b33b54
lesmadeit <lesmwendwa@gmail.com>
2023-09-21 11:08:01 +0300

    changes

--- server/index.js
+++ server/index.js
@@ -3,16 +3,18 @@ import React from 'react';
 import express from 'express';
 
 import {readFileSync} from 'fs';
+import {renderToString} from 'react-dom/server';
+
+import { App } from '../client/App';
 
 const app = new express();
 
 app.use(express.static("dist"));
 app.get('/',async (_req,res)=>{
-    const index = readFileSync('public/index.html','utf8')
-    res.send(index);
-    //res.send(
-     //   '<h1>REACTING</h1>'
-    //)
+    const index = readFileSync('public/index.html','utf8');
+    const rendered= renderToString(<App/>);
+    res.send(index.replace("{{rendered}}",rendered));
+    
 
 })
 
