f08a003eebc95dbb6098a13b464631577e9cbf2d
taylorhelene <taylorhelene09@gmail.com>
2023-10-04 14:24:23 +0300

    some good style

--- client/App.jsx
+++ client/App.jsx
@@ -4,8 +4,8 @@ import React from "react";
 export const App =({questions, answers, handleModifiedAnswerVotes})=>{
     return(
         <div className="container">
-            <header className="page-header">
-            <h1 className="jumbotron text-center text-primary">
+            <header className="page-header border border-0 border-primary">
+            <h1 className="jumbotron text-center text-primary m-5">
                 Question  and Answer tool 
             </h1>
             </header>
@@ -18,12 +18,12 @@ export const App =({questions, answers, handleModifiedAnswerVotes})=>{
                     <div>
                         {answers.filter(answer=> answer.questionId=== questionId).map(({content,upvotes,answerId})=>{
                             return(
-                                <div key={answerId}>
-                                    <span>
+                                <div key={answerId} className="row">
+                                    <span className=" col-sm-4">
                                         {content} - {upvotes}
                                     </span>
-                                    <button onClick={()=>handleModifiedAnswerVotes(answerId,1) } className="well btn btn-primary">+</button>
-                                    <button onClick={()=>handleModifiedAnswerVotes(answerId,-1) } className="well btn btn-primary">-</button>
+                                    <button onClick={()=>handleModifiedAnswerVotes(answerId,1) } className="rounded-lg btn btn-primary col-sm-2 m-5"> + </button>
+                                    <button onClick={()=>handleModifiedAnswerVotes(answerId,-1) } className="rounded-lg btn btn-primary col-sm-2 m-5"> - </button>
                                 </div>
                             )
                         })}
